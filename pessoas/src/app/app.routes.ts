01e195aac38b0878bece5ce7182745444fe3ee5a
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-04 21:18:34 -0300

    novas alterações, home/component adicionado.

--- pessoas/src/app/app.routes.ts
+++ pessoas/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Cadastro } from './cadastro/cadastro';
+import { HomeComponent } from './component/home-component/home-component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cadastro',
+        component: Cadastro
+    }
+];
