78546550b7f658454dba5edd5413a5a4fba0e973
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-04 21:42:47 -0300

    novas alterações

--- pessoas/src/app/cadastro/cadastro.ts
+++ pessoas/src/app/cadastro/cadastro.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PessoaService } from '../services/pessoa-service';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+export class Cadastro {
+  nome = ''
+  email = ''
+  cpf = ''
+  dataNascimento = ''
+
+  constructor(private pessoaService: PessoaService) { }
+
+  save() {
+    this.pessoaService.adicionar({
+      id: 1,
+      nome: this.nome,
+      cpf: this.cpf,
+      email: this.email,
+      dataNascimento: this.dataNascimento
+    })
+
+  }
+
+}
